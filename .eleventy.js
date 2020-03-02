cae08b824505b268b4a3c113721ebbb33b8bf1a3
Richard Herbert <richard@infoweb.co.uk>
2020-03-02 09:26:56 +0000

    Added dump filter, quiet mode and silent log level

--- .eleventy.js
+++ .eleventy.js
@@ -3,21 +3,28 @@ const yaml = require( 'js-yaml' );
 
 module.exports = function( eleventyConfig ) {
 	eleventyConfig.setTemplateFormats( 'html,md' );
+	eleventyConfig.setQuietMode( true );
 
 	eleventyConfig.addPassthroughCopy( 'assets' );
-	eleventyConfig.addPassthroughCopy( 'img' );
 	eleventyConfig.addPassthroughCopy( 'css' );
-	eleventyConfig.addPassthroughCopy( 'js' );
-	eleventyConfig.addPassthroughCopy( 'fonts' );
 	eleventyConfig.addPassthroughCopy( 'favicon' );
+	eleventyConfig.addPassthroughCopy( 'fonts' );
+	eleventyConfig.addPassthroughCopy( 'img' );
+	eleventyConfig.addPassthroughCopy( 'js' );
+
 	eleventyConfig.addPassthroughCopy( 'robots.txt' );
 	eleventyConfig.addPassthroughCopy( '404.html' );
 	eleventyConfig.addPassthroughCopy( 'google0807158b843a6325.html' );
 
+	eleventyConfig.addFilter( 'dump', function( anything ) {
+		console.log( 'dump:', anything );
+	} );
+
 	eleventyConfig.addDataExtension( 'yml', contents => yaml.safeLoad( contents ) );
 
 	eleventyConfig.setBrowserSyncConfig( {
-		ui: false,
-		ghostMode: false
+		ui: false
+		,ghostMode: false
+		,logLevel: 'silent'
 	} );
 };
